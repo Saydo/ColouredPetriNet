5f4073583614405096bf447bb78b9283354786ae
Saydo <saydos@mail.ru>
2016-07-13 20:11:43 +0300

    add main menu items

--- ColouredPetriNet/Gui/Forms/MainForm.Designer.cs
+++ ColouredPetriNet/Gui/Forms/MainForm.Designer.cs
@@ -9,10 +9,42 @@ namespace ColouredPetriNet.Gui.Forms
         {
             mnsMain = new MenuStrip();
             mniFile = new ToolStripMenuItem();
+            mniOpen = new ToolStripMenuItem();
+            mniSave = new ToolStripMenuItem();
+            mniSaveAs = new ToolStripMenuItem();
+            mniExit = new ToolStripMenuItem();
             mniMap = new ToolStripMenuItem();
+            mniSetMode = new ToolStripMenuItem();
+            mniSetModeView = new ToolStripMenuItem();
+            mniSetModeMove = new ToolStripMenuItem();
+            mniSetModeAddState = new ToolStripMenuItem();
+            mniSetModeAddRoundState = new ToolStripMenuItem();
+            mniSetModeAddImageState = new ToolStripMenuItem();
+            mniSetModeAddTransition = new ToolStripMenuItem();
+            mniSetModeAddRectangleTransition = new ToolStripMenuItem();
+            mniSetModeAddRhombTransition = new ToolStripMenuItem();
+            mniSetModeAddMarker = new ToolStripMenuItem();
+            mniSetModeAddRoundMarker = new ToolStripMenuItem();
+            mniSetModeAddRhombMarker = new ToolStripMenuItem();
+            mniSetModeAddTriangleMarker = new ToolStripMenuItem();
+            mniSetModeAddLink = new ToolStripMenuItem();
+            mniSetModeRemove = new ToolStripMenuItem();
+            mniSetModeRemoveMarker = new ToolStripMenuItem();
+            mniShowInfo = new ToolStripMenuItem();
+            mniShowStateInfo = new ToolStripMenuItem();
+            mniShowTransitionInfo = new ToolStripMenuItem();
+            mniShowMarkerInfo = new ToolStripMenuItem();
             mniView = new ToolStripMenuItem();
+            mniSelectionMode = new ToolStripMenuItem();
+            mniSelectionModeFull = new ToolStripMenuItem();
+            mniSelectionModePartial = new ToolStripMenuItem();
+            mniItemStyle = new ToolStripMenuItem();
+            mniStateStyle = new ToolStripMenuItem();
+            mniTransitionStyle = new ToolStripMenuItem();
+            mniMarkerStyle = new ToolStripMenuItem();
+            mniLinkStyle = new ToolStripMenuItem();
+            mniBackground = new ToolStripMenuItem();
             mniAbout = new ToolStripMenuItem();
-
             tlsMain = new ToolStrip();
             tsbView = new ToolStripButton();
             tsbMove = new ToolStripButton();
@@ -58,18 +90,270 @@ namespace ColouredPetriNet.Gui.Forms
             mniFile.Name = "mniFile";
             mniFile.Size = new Size(37, 20);
             mniFile.Text = "File";
+            mniFile.DropDownItems.AddRange(new ToolStripItem[] {
+                mniOpen,
+                mniSave,
+                mniSaveAs,
+                mniExit
+            });
+            //
+            // Open (ToolStripMenuItem)
+            //
+            mniOpen.Name = "mniOpen";
+            mniOpen.Size = new Size(152, 22);
+            mniOpen.Text = "Open";
+            //
+            // Save (ToolStripMenuItem)
+            //
+            mniSave.Name = "mniSave";
+            mniSave.Size = new Size(152, 22);
+            mniSave.Text = "Save";
+            //
+            // SaveAs (ToolStripMenuItem)
+            //
+            mniSaveAs.Name = "mniSaveAs";
+            mniSaveAs.Size = new Size(152, 22);
+            mniSaveAs.Text = "Save As...";
+            //
+            // Exit (ToolStripMenuItem)
+            //
+            mniExit.Name = "mniExit";
+            mniExit.Size = new Size(152, 22);
+            mniExit.Text = "Exit";
             //
             // Map (ToolStripMenuItem)
             //
             mniMap.Name = "mniMap";
             mniMap.Size = new Size(43, 20);
             mniMap.Text = "Map";
+            mniMap.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSetMode,
+                mniShowInfo
+            });
+            //
+            // SetMode (ToolStripMenuItem)
+            //
+            mniSetMode.Name = "mniSetMode";
+            mniSetMode.Size = new Size(152, 22);
+            mniSetMode.Text = "Set Mode";
+            mniSetMode.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSetModeView,
+                mniSetModeMove,
+                mniSetModeAddState,
+                mniSetModeAddTransition,
+                mniSetModeAddMarker,
+                mniSetModeAddLink,
+                mniSetModeRemove,
+                mniSetModeRemoveMarker
+            });
+            //
+            // SetModeView (ToolStripMenuItem)
+            //
+            mniSetModeView.Name = "mniSetModeView";
+            mniSetModeView.Size = new Size(152, 22);
+            mniSetModeView.Text = "View";
+            //
+            // SetModeMove (ToolStripMenuItem)
+            //
+            mniSetModeMove.Name = "mniSetModeMove";
+            mniSetModeMove.Size = new Size(152, 22);
+            mniSetModeMove.Text = "Move";
+            //
+            // SetModeAddState (ToolStripMenuItem)
+            //
+            mniSetModeAddState.Name = "mniSetModeAddState";
+            mniSetModeAddState.Size = new Size(152, 22);
+            mniSetModeAddState.Text = "Add State";
+            mniSetModeAddState.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSetModeAddRoundState,
+                mniSetModeAddImageState
+            });
+            //
+            // SetModeAddRoundState (ToolStripMenuItem)
+            //
+            mniSetModeAddRoundState.Name = "mniSetModeAddRoundState";
+            mniSetModeAddRoundState.Size = new Size(152, 22);
+            mniSetModeAddRoundState.Text = "Round State";
+            //
+            // SetModeAddImageState (ToolStripMenuItem)
+            //
+            mniSetModeAddImageState.Name = "mniSetModeAddImageState";
+            mniSetModeAddImageState.Size = new Size(152, 22);
+            mniSetModeAddImageState.Text = "Image State";
+            //
+            // SetModeAddTransition (ToolStripMenuItem)
+            //
+            mniSetModeAddTransition.Name = "mniSetModeAddTransition";
+            mniSetModeAddTransition.Size = new Size(152, 22);
+            mniSetModeAddTransition.Text = "Add Transition";
+            mniSetModeAddTransition.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSetModeAddRectangleTransition,
+                mniSetModeAddRhombTransition
+            });
+            //
+            // SetModeAddRectangleTransition (ToolStripMenuItem)
+            //
+            mniSetModeAddRectangleTransition.Name = "mniSetModeAddRectangleTransition";
+            mniSetModeAddRectangleTransition.Size = new Size(152, 22);
+            mniSetModeAddRectangleTransition.Text = "Rectangle Transition";
+            //
+            // SetModeAddRhombTransition (ToolStripMenuItem)
+            //
+            mniSetModeAddRhombTransition.Name = "mniSetModeAddRhombTransition";
+            mniSetModeAddRhombTransition.Size = new Size(152, 22);
+            mniSetModeAddRhombTransition.Text = "Rhomb Transition";
+            //
+            // SetModeAddMarker (ToolStripMenuItem)
+            //
+            mniSetModeAddMarker.Name = "mniSetModeAddMarker";
+            mniSetModeAddMarker.Size = new Size(152, 22);
+            mniSetModeAddMarker.Text = "Add Marker";
+            mniSetModeAddMarker.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSetModeAddRoundMarker,
+                mniSetModeAddRhombMarker,
+                mniSetModeAddTriangleMarker
+            });
+            //
+            // SetModeAddRoundMarker (ToolStripMenuItem)
+            //
+            mniSetModeAddRoundMarker.Name = "mniSetModeAddRoundMarker";
+            mniSetModeAddRoundMarker.Size = new Size(152, 22);
+            mniSetModeAddRoundMarker.Text = "Round Marker";
+            //
+            // SetModeAddRhombMarker (ToolStripMenuItem)
+            //
+            mniSetModeAddRhombMarker.Name = "mniSetModeAddRhombMarker";
+            mniSetModeAddRhombMarker.Size = new Size(152, 22);
+            mniSetModeAddRhombMarker.Text = "Rhomb Marker";
+            //
+            // SetModeAddTriangleMarker (ToolStripMenuItem)
+            //
+            mniSetModeAddTriangleMarker.Name = "mniSetModeAddTriangleMarker";
+            mniSetModeAddTriangleMarker.Size = new Size(152, 22);
+            mniSetModeAddTriangleMarker.Text = "Triangle Marker";
+            //
+            // SetModeAddLink (ToolStripMenuItem)
+            //
+            mniSetModeAddLink.Name = "mniSetModeAddLink";
+            mniSetModeAddLink.Size = new Size(152, 22);
+            mniSetModeAddLink.Text = "Add Link";
+            //
+            // SetModeRemove (ToolStripMenuItem)
+            //
+            mniSetModeRemove.Name = "mniSetModeRemove";
+            mniSetModeRemove.Size = new Size(152, 22);
+            mniSetModeRemove.Text = "Remove";
+            //
+            // SetModeRemoveMarker (ToolStripMenuItem)
+            //
+            mniSetModeRemoveMarker.Name = "mniSetModeRemoveMarker";
+            mniSetModeRemoveMarker.Size = new Size(152, 22);
+            mniSetModeRemoveMarker.Text = "Remove Marker";
+            //
+            // ShowInfo (ToolStripMenuItem)
+            //
+            mniShowInfo.Name = "mniShowInfo";
+            mniShowInfo.Size = new Size(152, 22);
+            mniShowInfo.Text = "Show Info";
+            mniShowInfo.DropDownItems.AddRange(new ToolStripItem[] {
+                mniShowStateInfo,
+                mniShowTransitionInfo,
+                mniShowMarkerInfo
+            });
+            //
+            // ShowStateInfo (ToolStripMenuItem)
+            //
+            mniShowStateInfo.Name = "mniShowStateInfo";
+            mniShowStateInfo.Size = new Size(152, 22);
+            mniShowStateInfo.Text = "About State";
+            //
+            // ShowTransitionInfo (ToolStripMenuItem)
+            //
+            mniShowTransitionInfo.Name = "mniShowTransitionInfo";
+            mniShowTransitionInfo.Size = new Size(152, 22);
+            mniShowTransitionInfo.Text = "About Transition";
+            //
+            // ShowMarkerInfo (ToolStripMenuItem)
+            //
+            mniShowMarkerInfo.Name = "mniShowMarkerInfo";
+            mniShowMarkerInfo.Size = new Size(152, 22);
+            mniShowMarkerInfo.Text = "About Marker";
             //
             // View (ToolStripMenuItem)
             //
             mniView.Name = "mniView";
             mniView.Size = new Size(44, 20);
             mniView.Text = "View";
+            mniView.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSelectionMode,
+                mniItemStyle,
+                mniBackground
+            });
+            //
+            // SelectionMode (ToolStripMenuItem)
+            //
+            mniSelectionMode.Name = "mniSelectionMode";
+            mniSelectionMode.Size = new Size(152, 22);
+            mniSelectionMode.Text = "Selection Mode";
+            mniSelectionMode.DropDownItems.AddRange(new ToolStripItem[] {
+                mniSelectionModeFull,
+                mniSelectionModePartial
+            });
+            //
+            // SelectionModeFull (ToolStripMenuItem)
+            //
+            mniSelectionModeFull.Name = "mniSelectionModeFull";
+            mniSelectionModeFull.Size = new Size(152, 22);
+            mniSelectionModeFull.Text = "Full";
+            //
+            // SelectionModePartial (ToolStripMenuItem)
+            //
+            mniSelectionModePartial.Name = "mniSelectionModePartial";
+            mniSelectionModePartial.Size = new Size(152, 22);
+            mniSelectionModePartial.Text = "Partial";
+            mniSelectionModePartial.Checked = true;
+            //
+            // ItemStyle (ToolStripMenuItem)
+            //
+            mniItemStyle.Name = "mniItemStyle";
+            mniItemStyle.Size = new Size(152, 22);
+            mniItemStyle.Text = "Item Style";
+            mniItemStyle.DropDownItems.AddRange(new ToolStripItem[] {
+                mniStateStyle,
+                mniTransitionStyle,
+                mniMarkerStyle,
+                mniLinkStyle
+            });
+            //
+            // StateStyle (ToolStripMenuItem)
+            //
+            mniStateStyle.Name = "mniStateStyle";
+            mniStateStyle.Size = new Size(152, 22);
+            mniStateStyle.Text = "States";
+            //
+            // TransitionStyle (ToolStripMenuItem)
+            //
+            mniTransitionStyle.Name = "mniTransitionStyle";
+            mniTransitionStyle.Size = new Size(152, 22);
+            mniTransitionStyle.Text = "Transitions";
+            //
+            // MarkerStyle (ToolStripMenuItem)
+            //
+            mniMarkerStyle.Name = "mniMarkerStyle";
+            mniMarkerStyle.Size = new Size(152, 22);
+            mniMarkerStyle.Text = "Markers";
+            //
+            // mniLinkStyle (ToolStripMenuItem)
+            //
+            mniLinkStyle.Name = "mniLinkStyle";
+            mniLinkStyle.Size = new Size(152, 22);
+            mniLinkStyle.Text = "Links";
+            //
+            // Background (ToolStripMenuItem)
+            //
+            mniBackground.Name = "mniBackground";
+            mniBackground.Size = new Size(152, 22);
+            mniBackground.Text = "Background";
             //
             // About (ToolStripMenuItem)
             //
@@ -287,9 +571,48 @@ namespace ColouredPetriNet.Gui.Forms
         }
 
         private MenuStrip mnsMain;
+
         private ToolStripMenuItem mniFile;
+        private ToolStripMenuItem mniOpen;
+        private ToolStripMenuItem mniSave;
+        private ToolStripMenuItem mniSaveAs;
+        private ToolStripMenuItem mniExit;
+
         private ToolStripMenuItem mniMap;
+        private ToolStripMenuItem mniSetMode;
+
+        private ToolStripMenuItem mniSetModeView;
+        private ToolStripMenuItem mniSetModeMove;
+        private ToolStripMenuItem mniSetModeAddState;
+        private ToolStripMenuItem mniSetModeAddRoundState;
+        private ToolStripMenuItem mniSetModeAddImageState;
+        private ToolStripMenuItem mniSetModeAddTransition;
+        private ToolStripMenuItem mniSetModeAddRectangleTransition;
+        private ToolStripMenuItem mniSetModeAddRhombTransition;
+        private ToolStripMenuItem mniSetModeAddMarker;
+        private ToolStripMenuItem mniSetModeAddRoundMarker;
+        private ToolStripMenuItem mniSetModeAddRhombMarker;
+        private ToolStripMenuItem mniSetModeAddTriangleMarker;
+        private ToolStripMenuItem mniSetModeAddLink;
+        private ToolStripMenuItem mniSetModeRemove;
+        private ToolStripMenuItem mniSetModeRemoveMarker;
+
+        private ToolStripMenuItem mniShowInfo;
+        private ToolStripMenuItem mniShowStateInfo;
+        private ToolStripMenuItem mniShowTransitionInfo;
+        private ToolStripMenuItem mniShowMarkerInfo;
+
         private ToolStripMenuItem mniView;
+        private ToolStripMenuItem mniSelectionMode;
+        private ToolStripMenuItem mniSelectionModeFull;
+        private ToolStripMenuItem mniSelectionModePartial;
+        private ToolStripMenuItem mniItemStyle;
+        private ToolStripMenuItem mniStateStyle;
+        private ToolStripMenuItem mniTransitionStyle;
+        private ToolStripMenuItem mniMarkerStyle;
+        private ToolStripMenuItem mniLinkStyle;
+        private ToolStripMenuItem mniBackground;
+
         private ToolStripMenuItem mniAbout;
         private ToolStrip tlsMain;
         private ToolStripButton tsbView;
